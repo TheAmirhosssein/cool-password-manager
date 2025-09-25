53b877d236af6f8e48800271f32f9c69f0c9c140
amirhossein <amirhosseinmobini9@gmail.com>
2025-09-25 14:33:40 +0330

    feat(account): :sparkles: account entity created

---
+++ internal/app/account/entity/account.go
@@ -0,0 +1,12 @@
+package entity
+
+import "github.com/TheAmirhosssein/cool-password-manage/internal/utils/base"
+
+type Account struct {
+	Entity    base.Entity
+	Username  string
+	Email     string
+	FirstName string
+	LastName  string
+	Password  string
+}
