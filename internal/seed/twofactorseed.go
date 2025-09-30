fa5c0df164bc3f075752b70b08af4132141bca11
amirhossein <amirhosseinmobini9@gmail.com>
2025-09-30 18:20:41 +0330

    feat(seed): :sparkles: two factor seed added into project

---
+++ internal/seed/twofactorseed.go
@@ -0,0 +1,28 @@
+package seed
+
+import (
+	"context"
+	"time"
+
+	"github.com/TheAmirhosssein/cool-password-manage/internal/app/account/entity"
+	"github.com/redis/go-redis/v9"
+)
+
+const (
+	idJohnDoeTwoFactor = "john_doe_two_factor"
+)
+
+var (
+	TwoFactorJohnDoe = entity.TwoFactor{
+		ID:       idJohnDoeTwoFactor,
+		Username: AccountJohnDoe.Username,
+		Duration: time.Minute * 2,
+	}
+)
+
+func CreateTowFactorSeed(ctx context.Context, rdb *redis.Client) {
+	err := rdb.Set(ctx, string(TwoFactorJohnDoe.ID), TwoFactorJohnDoe.Username, TwoFactorJohnDoe.Duration).Err()
+	if err != nil {
+		panic(err)
+	}
+}
