98b072d71233e9bcfe82674fe219e6399195f2bd
amirhossein <amirhosseinmobini9@gmail.com>
2025-09-30 18:19:01 +0330

    feat(account): :sparkles: duration added into two factor

--- internal/app/account/entity/twofactor.go
+++ internal/app/account/entity/twofactor.go
@@ -1,8 +1,13 @@
 package entity
 
-import "github.com/TheAmirhosssein/cool-password-manage/internal/types"
+import (
+	"time"
+
+	"github.com/TheAmirhosssein/cool-password-manage/internal/types"
+)
 
 type TwoFactor struct {
 	ID       types.CacheID
 	Username string
+	Duration time.Duration
 }
