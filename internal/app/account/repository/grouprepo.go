f85e7f353fb520da8f46b580789459ae8ee69c9d
amirhossein <amirhosseinmobini9@gmail.com>
2025-10-04 16:20:36 +0330

    feat(account): :sparkles: add account added into group repo

--- internal/app/account/repository/grouprepo.go
+++ internal/app/account/repository/grouprepo.go
@@ -2,14 +2,18 @@ package repository
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	"github.com/TheAmirhosssein/cool-password-manage/internal/app/account/entity"
+	"github.com/TheAmirhosssein/cool-password-manage/internal/types"
 	"github.com/TheAmirhosssein/cool-password-manage/pkg/log"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type GroupRepository interface {
 	Create(ctx context.Context, group entity.Group) error
+	AddAccount(ctx context.Context, groupID types.ID, accounts []entity.Account) error
 }
 
 type groupRepo struct {
@@ -30,4 +34,28 @@ func (repo groupRepo) Create(ctx context.Context, group entity.Group) error {
 	}
 
 	return nil
+}
+
+func (repo groupRepo) AddAccount(ctx context.Context, groupID types.ID, accounts []entity.Account) error {
+	var (
+		values []string
+		args   []any
+	)
+
+	args = append(args, groupID)
+
+	for i, account := range accounts {
+		placeholder := fmt.Sprintf("($1, $%d)", i+2)
+		values = append(values, placeholder)
+
+		args = append(args, account.Entity.ID)
+	}
+
+	query := fmt.Sprintf(
+		"INSERT INTO groups_accounts (group_id, account_id) VALUES %s",
+		strings.Join(values, ","),
+	)
+
+	_, err := repo.db.Exec(ctx, query, args...)
+	return err
 }
--- internal/app/account/repository/grouprepo_test.go
+++ internal/app/account/repository/grouprepo_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/TheAmirhosssein/cool-password-manage/internal/app/account/entity"
 	"github.com/TheAmirhosssein/cool-password-manage/internal/app/account/repository"
 	"github.com/TheAmirhosssein/cool-password-manage/internal/seed"
+	"github.com/TheAmirhosssein/cool-password-manage/internal/types"
 	"github.com/TheAmirhosssein/cool-password-manage/internal/utils/base"
 	"github.com/stretchr/testify/require"
 )
@@ -59,6 +60,67 @@ func TestGroupRepository_Create(t *testing.T) {
 				require.Error(t, err)
 			} else {
 				require.NoError(t, err)
+				require.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestGroupRepository_AddAccount(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+	repo := repository.NewGroupRepository(pgTestSuite.db)
+
+	testcases := []struct {
+		name     string
+		groupID  types.ID
+		accounts []entity.Account
+		wantErr  bool
+	}{
+		{
+			name:    "add single account to group",
+			groupID: seed.GroupBrockhampton.Entity.ID,
+			accounts: []entity.Account{
+				seed.AccountJohnDoe,
+			},
+			wantErr: false,
+		},
+		{
+			name:    "add multiple accounts to group",
+			groupID: seed.GroupRadiohead.Entity.ID,
+			accounts: []entity.Account{
+				seed.AccountJohnDoe,
+				seed.AccountEarl,
+			},
+			wantErr: false,
+		},
+		{
+			name:    "invalid group id",
+			groupID: -1,
+			accounts: []entity.Account{
+				seed.AccountJohnDoe,
+			},
+			wantErr: true,
+		},
+		{
+			name:    "invalid account id",
+			groupID: seed.GroupBrockhampton.Entity.ID,
+			accounts: []entity.Account{
+				{Entity: base.Entity{ID: -1}},
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tc := range testcases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			err := repo.AddAccount(ctx, tc.groupID, tc.accounts)
+			if tc.wantErr {
+				require.Error(t, err)
+			} else {
 				require.NoError(t, err)
 			}
 		})
