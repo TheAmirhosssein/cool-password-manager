ebddc6f07ca9ef08b5ce95e38dc5dac7cf46e45e
amirhossein <amirhosseinmobini9@gmail.com>
2026-01-07 22:30:47 +0330

    feat(account): :sparkles: opaque adaptor authentication added

---
+++ internal/infrastructure/opaque/opaque.go
@@ -0,0 +1,7 @@
+package opaque
+
+type OpaqueService interface {
+	Init() error
+	RegisterInit(message []byte) (response []byte, credID []byte, err error)
+	RegisterFinalize(message, credID []byte, username string) ([]byte, error)
+}
