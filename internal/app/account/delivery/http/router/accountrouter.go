dc918ccf2c2e6607dd4fb56f09edb1a7a8b63d60
amirhossein <amirhosseinmobini9@gmail.com>
2026-02-02 20:07:23 +0330

    feat(account): :sparkles: error return added into account router

--- internal/app/account/delivery/http/router/accountrouter.go
+++ internal/app/account/delivery/http/router/accountrouter.go
@@ -3,6 +3,7 @@ package router
 import (
 	"github.com/TheAmirhosssein/cool-password-manage/config"
 	"github.com/TheAmirhosssein/cool-password-manage/internal/app/account/repository"
+	"github.com/TheAmirhosssein/cool-password-manage/internal/infrastructure/opaque"
 	"github.com/TheAmirhosssein/cool-password-manage/internal/infrastructure/totp"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
@@ -11,18 +12,24 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-func AccountRouter(server *gin.Engine, conf *config.Config, db *pgxpool.Pool, redis *redis.Client) {
+func AccountRouter(server *gin.Engine, conf *config.Config, db *pgxpool.Pool, redis *redis.Client) error {
 	store := cookie.NewStore([]byte(conf.SecretKey))
 	server.Use(sessions.Sessions("mysession", store))
 
 	// Create auth
 	accountRepo := repository.NewAccountRepository(db)
 	twoFactorRepo := repository.NewTwoFactorRepository(redis)
+	registrationRepo := repository.NewRegistrationRepository(redis)
 	groupRepo := repository.NewGroupRepository(db)
 	authenticator := totp.NewAuthenticatorAdaptor(conf.Name)
+	opaqueAdaptor, err := opaque.New(conf)
+	if err != nil {
+		return err
+	}
 
 	// Register routers
-	authRouter(server, accountRepo, twoFactorRepo, authenticator, conf)
+	authRouter(server, accountRepo, twoFactorRepo, registrationRepo, authenticator, opaqueAdaptor, conf)
 	meRouter(server, groupRepo, accountRepo, conf)
 	groupRouter(server, groupRepo, accountRepo, conf)
+	return nil
 }
