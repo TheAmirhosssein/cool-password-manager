d957e1af5f93dfdd70d8a3c7eb55d43a8736b68a
amirhossein <amirhosseinmobini9@gmail.com>
2026-02-04 20:04:09 +0330

    feat(infrastructure): :sparkles: username added into login init

--- internal/infrastructure/opaque/opaque.go
+++ internal/infrastructure/opaque/opaque.go
@@ -4,6 +4,6 @@ type OpaqueService interface {
 	Init() error
 	RegisterInit(message []byte) (response []byte, credID []byte, err error)
 	RegisterFinalize(message, credID []byte, username string) ([]byte, error)
-	LoginInit(message, userRecord []byte) ([]byte, error)
+	LoginInit(message, userRecord []byte, username string) ([]byte, error)
 	LoginFinalize(message []byte) ([]byte, error)
 }
